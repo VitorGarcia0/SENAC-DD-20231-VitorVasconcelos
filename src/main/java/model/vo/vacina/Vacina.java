626867470e81f6aec5a6f0ab4049eb8205711529
Acer <Acer@DESKTOP-BRCFFLU>
2023-03-26 23:57:56 -0300

    Commit da VACINA e PESSOA
    DAO's e VO's

--- src/main/java/model/vo/vacina/Vacina.java
+++ src/main/java/model/vo/vacina/Vacina.java
@@ -1,13 +1,61 @@
 package model.vo.vacina;
 
-import java.time.format.DateTimeFormatter;
+import java.sql.Date;
 
 public class Vacina {
 	
+	private Integer id;
 	private String paisOrigem;
 	private String estagioPesquisa;
-	private DateTimeFormatter dataInicioPesquisa;
+	private Date dataInicioPesquisa;
 	private String nomeResponsavel;
 	
+	
+	public Vacina(Integer id, String paisOrigem, String estagioPesquisa, Date dataInicioPesquisa,
+			String nomeResponsavel) {
+		super();
+		this.id = id;
+		this.paisOrigem = paisOrigem;
+		this.estagioPesquisa = estagioPesquisa;
+		this.dataInicioPesquisa = dataInicioPesquisa;
+		this.nomeResponsavel = nomeResponsavel;
+	}
+	public Vacina() {
+		super();
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getPaisOrigem() {
+		return paisOrigem;
+	}
+	public void setPaisOrigem(String paisOrigem) {
+		this.paisOrigem = paisOrigem;
+	}
+	public String getEstagioPesquisa() {
+		return estagioPesquisa;
+	}
+	public void setEstagioPesquisa(String estagioPesquisa) {
+		this.estagioPesquisa = estagioPesquisa;
+	}
+	public Date getDataInicioPesquisa() {
+		return dataInicioPesquisa;
+	}
+	public void setDataInicioPesquisa(Date dataInicioPesquisa) {
+		this.dataInicioPesquisa = dataInicioPesquisa;
+	}
+	public String getNomeResponsavel() {
+		return nomeResponsavel;
+	}
+	public void setNomeResponsavel(String nomeResponsavel) {
+		this.nomeResponsavel = nomeResponsavel;
+	}
+	
+	
+	
+
 
 }
