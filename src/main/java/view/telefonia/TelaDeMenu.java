e08372706fddec19f75d252b295c162cd97caf91
vitor.vasconcelos <vitor.vasconcelos@FLE207D30.alunos.sc.senac.br>
2023-05-15 11:01:36 -0300

    Icones para Utilizar no Menu principal da Telefone
    Criacão dos Paineis

--- src/main/java/view/telefonia/TelaDeMenu.java
+++ src/main/java/view/telefonia/TelaDeMenu.java
@@ -6,6 +6,7 @@ import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.ImageIcon;
 
 public class TelaDeMenu {
 
@@ -47,10 +48,40 @@ public class TelaDeMenu {
 		frmPrincipal.setJMenuBar(menuBar);
 		
 		JMenu mnClientes = new JMenu("Clientes");
+		mnClientes.setIcon(new ImageIcon(TelaDeMenu.class.getResource("/icones/IconeCliente.png")));
 		menuBar.add(mnClientes);
 		
-		JMenuItem mntmNewMenuItem = new JMenuItem("Cadastro");
-		mnClientes.add(mntmNewMenuItem);
+		JMenuItem mntmClienteCadastro = new JMenuItem("Cadastro");
+		mntmClienteCadastro.setIcon(new ImageIcon(TelaDeMenu.class.getResource("/icones/IconeListagemClientes.png")));
+		mnClientes.add(mntmClienteCadastro);
+		
+		JMenuItem mntmListagemCliente = new JMenuItem("Listagem");
+		mntmListagemCliente.setIcon(new ImageIcon(TelaDeMenu.class.getResource("/icones/IconeListagemClientes.png")));
+		mnClientes.add(mntmListagemCliente);
+		
+		JMenu mnTelefone = new JMenu("Telefone");
+		mnTelefone.setIcon(new ImageIcon(TelaDeMenu.class.getResource("/icones/IconeTelefone01.png")));
+		menuBar.add(mnTelefone);
+		
+		JMenuItem mntmTelefoneCadastro = new JMenuItem("Tel Cadastro");
+		mntmTelefoneCadastro.setIcon(new ImageIcon(TelaDeMenu.class.getResource("/icones/TelefoneAdicionar.png")));
+		mnTelefone.add(mntmTelefoneCadastro);
+		
+		JMenuItem mntmListagemTelefone = new JMenuItem("Listagem");
+		mntmListagemTelefone.setIcon(new ImageIcon(TelaDeMenu.class.getResource("/icones/IconeTelefone.png")));
+		mnTelefone.add(mntmListagemTelefone);
+		
+		JMenu mnEndereco = new JMenu("Endereço");
+		mnEndereco.setIcon(new ImageIcon(TelaDeMenu.class.getResource("/icones/EnderecoIcone.png")));
+		menuBar.add(mnEndereco);
+		
+		JMenuItem mntmEnderecoCadastro = new JMenuItem("Cadastro");
+		mntmEnderecoCadastro.setIcon(new ImageIcon(TelaDeMenu.class.getResource("/icones/icons8-endereço.gif")));
+		mnEndereco.add(mntmEnderecoCadastro);
+		
+		JMenuItem mntmEnderecoListagem = new JMenuItem("Listagem");
+		mntmEnderecoListagem.setIcon(new ImageIcon(TelaDeMenu.class.getResource("/icones/EnderecoListagem.png")));
+		mnEndereco.add(mntmEnderecoListagem);
 		
 		}
 
