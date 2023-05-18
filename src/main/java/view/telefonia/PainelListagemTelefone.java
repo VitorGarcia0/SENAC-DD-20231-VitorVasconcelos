ae2733fe4b5cced4bb19dfe070711c4711f76540
vitor.vasconcelos <vitor.vasconcelos@FLE207D13.alunos.sc.senac.br>
2023-05-18 11:13:12 -0300

    Alteração no painel de telefones

--- src/main/java/view/telefonia/PainelListagemTelefone.java
+++ src/main/java/view/telefonia/PainelListagemTelefone.java
@@ -2,23 +2,80 @@ package view.telefonia;
 
 import javax.swing.JPanel;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.FormSpecs;
+import com.jgoodies.forms.layout.RowSpec;
+
+import controller.ClienteController;
+import controller.TelefoneController;
+import model.vo.telefonia.ClienteVO;
+import model.vo.telefonia.TelefoneVO;
+
 import java.awt.Color;
 import java.awt.LayoutManager;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class PainelListagemTelefone extends JPanel {
-	private JTable table_1;
-
-	/**
-	 * Create the panel.
-	 */
-	public PainelListagemTelefone() {
-		setLayout(null);
+	private JTable tblTelefones;
+	private String[] nomesColunas = {  "Número Completo", "Tipo", "Nome Cliente", "Ativo?" };
+	private List<TelefoneVO> telefones = new ArrayList<>();
+	private TelefoneController controller = new TelefoneController();
+	
+	private void limparTabela() {
+		tblTelefones.setModel(new DefaultTableModel(new Object[][] { nomesColunas, }, nomesColunas));
+	}
+	
+	private void atualizarTabela() {
+		this.limparTabela();
 		
-		table_1 = new JTable();
-		table_1.setBounds(0, 0, 0, 0);
-		add(table_1);
+		DefaultTableModel model = (DefaultTableModel) tblTelefones.getModel();
 
+		ClienteController clienteController = new ClienteController();
+		for (TelefoneVO t : this.telefones) {
+			//TODO melhorar para usar DTO
+			String nomeCliente = "Sem cliente";
+			if(t.getIdCliente() != null) {
+				ClienteVO clienteBuscado = clienteController.consultarPorId(t.getIdCliente());
+				
+				if(clienteBuscado != null) {
+					nomeCliente = clienteBuscado.getNome();
+				}
+			}
+			
+			Object[] novaLinhaDaTabela = new Object[4];
+			novaLinhaDaTabela[0] = "("+ t.getDdd() + ") " + t.getNumero();
+			novaLinhaDaTabela[1] = t.isMovel() ? "Móvel" : "Fixo";
+			novaLinhaDaTabela[2] = nomeCliente;
+			novaLinhaDaTabela[3] = t.isAtivo() ? "Sim" : "Não";
+
+			model.addRow(novaLinhaDaTabela);
+		}
+	}
+	
+	private void buscarTelefones() {
+		this.telefones = controller.consultarTodos();
+		this.atualizarTabela();
+	}
+	
+	public PainelListagemTelefone() {
+		setLayout(new FormLayout(new ColumnSpec[] {
+				FormSpecs.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),},
+			new RowSpec[] {
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("default:grow"),}));
+		
+		
+		tblTelefones = new JTable();
+		add(tblTelefones, "2, 4, fill, fill");
+		buscarTelefones();
 	}
 
 }
